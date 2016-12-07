1327939aea9b51ece1a1c27bd5e734076e40c3d9
Moa Bullon <moabullon@Moas-MacBook-Pro.local>
2016-12-07 08:17:41 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,131 @@
+
+
+// 1. ------------------------------------------------------------ //
+
+// Create a simple constructor function called `Foo` and create a new
+// instance from it called `foo`.
+
+// -- ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ - Your Answer - ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ -- //
+
+function Foo(name) {
+  this.name = name;
+}
+
+var foo = new Foo;
+
+// -- ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ -- //
+
+// ==== Validating =============================================== //
+
+console.assert(foo instanceof Foo);
+
+// --------------------------------------------------------------- //
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+// 2. ------------------------------------------------------------ //
+
+// Create a constructor function called `Dog` that sets a property
+// on itself within the constructor. The property should be called
+// `says` and the value should be `life is ruff`
+
+// -- ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ - Your Answer - ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ -- //
+
+function Dog () {
+  this.says = "life is ruff"
+}
+
+var doggy = new Dog
+
+// -- ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ -- //
+
+// ==== Validating =============================================== //
+
+console.assert(new Dog().says === "life is ruff");
+
+// --------------------------------------------------------------- //
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+// 3. ------------------------------------------------------------ //
+
+// Create a constructor function called `Cat` that has a method on
+// its prototype called `growl` that returns the string `meow`.
+// create an instance of this called `cat`
+
+// -- ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ - Your Answer - ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ -- //
+ function Cat () {
+}
+ Cat.prototype.growl = function (){
+   return "meow"
+ }
+
+
+
+var cat = new Cat
+
+// -- ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ -- //
+
+// ==== Validating =============================================== //
+
+console.assert(cat instanceof Cat);
+console.assert(cat.growl() === "meow");
+
+// --------------------------------------------------------------- //
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+// 4. ------------------------------------------------------------ //
+
+// Create a constructor called `KeepSecret` which should accept a parameter
+// called `secret`. It should store
+// `secret` in a private variable (use a closure). Add a method to the
+// prototype that is called `squeal` that returns the secret string.
+
+// -- ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ - Your Answer - ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ -- //
+
+
+
+// -- ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ -- //
+
+// ==== Validating =============================================== //
+
+let mySecret = "My class rocks!";
+let dontTellNobody = new KeepSecret(mySecret);
+console.assert(dontTellNobody.squeal() === mySecret);
+
+// --------------------------------------------------------------- //
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+// 5. ------------------------------------------------------------ //
+
+// Create a class called `Key`. Create another class
+// called `Safe`. Make the Safe constructor take 2 arguments. The
+// first argument can be any piece if data to keep safe. This must
+// be stored using a private variable like you did with KeepSecret.
+// The 2nd param to the `Safe` constructor needs to be an instance
+// of `Key` you need to store it privately as well. Add a function
+// to the Safe prototype called `unlock` that accepts a key. If the
+// key matches the key that was used to create the Safe; then return
+// the secret data.
+
+// -- ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ - Your Answer - ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ -- //
+
+
+
+// -- ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ -- //
+
+// ==== Validating =============================================== //
+
+let sensitive = "shhhhh!";
+let rightKey  = new Key();
+let wrongKey  = new Key();
+let safe      = new Safe(sensitive, rightKey);
+
+console.assert(safe.unlock(wrongKey) !== sensitive);
+console.assert(safe.unlock(rightKey) === sensitive);
